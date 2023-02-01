3dd49a3324ed02721bfcd7356f93a08108100102
mlatysheva <mlatysheva@gmail.com>
2023-02-01 11:13:51 +0100

    feat: add dotenv and take port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+import { cwd } from 'node:process';
+
+dotenv.config({ path: resolve(cwd(), '.env') });
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
