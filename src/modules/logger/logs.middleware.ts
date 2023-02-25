0fb00516b12784d76afbd91ad99584a76768490d
mlatysheva <mlatysheva@gmail.com>
2023-02-25 14:45:53 +0100

    feat: add logging of url, query params, body and status code

--- src/modules/logger/logs.middleware.ts
+++ src/modules/logger/logs.middleware.ts
@@ -1,16 +1,20 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { EOL } from 'node:os';
 
 @Injectable()
 export class LogsMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction) {
-    response.on('finish', () => {
-      const { method, originalUrl, query, body } = request;
-      const { statusCode, statusMessage } = response;
+    const { method, originalUrl: url, query, body } = request;
 
-      const message = `${method} ${originalUrl} ${query} ${body} ${statusCode} ${statusMessage}`;
+    response.on('finish', () => {
+      const { statusCode, statusMessage } = response;
+      const message = `${method}${EOL} ${url}${EOL} query:
+        ${JSON.stringify(query)}${EOL} body: ${JSON.stringify(
+        body,
+      )}${EOL} status: ${statusCode}${EOL} message: ${statusMessage}${EOL}`;
 
       if (statusCode >= 500) {
         return this.logger.error(message);
