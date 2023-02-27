2f8796d17f9d194eced992343307eb18e8707b06
mlatysheva <mlatysheva@gmail.com>
2023-02-27 10:13:52 +0100

    fix: make global authGuard work

--- src/migrations/1677481724856-migration.ts
+++ src/migrations/1677481724856-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class migration1677419679174 implements MigrationInterface {
-    name = 'migration1677419679174'
+export class migration1677481724856 implements MigrationInterface {
+    name = 'migration1677481724856'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "artist" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "grammy" boolean NOT NULL, CONSTRAINT "PK_55b76e71568b5db4d01d3e394ed" PRIMARY KEY ("id"))`);
